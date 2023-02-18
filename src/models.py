38456eb2bea5409fe32f870ad65da9d68203ba10
noedavico <noedavico@hotmail.com>
2023-02-18 12:11:19 +0000

    models ok

--- src/models.py
+++ src/models.py
@@ -2,14 +2,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    nombre = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    fecha_suscripcion =  db.Column(db.String(250), nullable=False)
+    apellido =  db.Column(db.String(250), nullable=False)
+    email =  db.Column(db.String(250), nullable=False)
+    favoritos = db.relationship('Favoritos',backref='user', lazy=True)
+    
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
@@ -17,3 +21,56 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+        
+class Personajes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(250), nullable=False)
+    altura = db.Column(db.String(250), nullable=False)
+    genero =  db.Column(db.String(250), nullable=False)
+    peso =  db.Column(db.String(250), nullable=False)
+    favoritos = db.relationship('Favoritos',backref='personajes', lazy=True)        
+    
+    def __repr__(self):
+        return '<Personajes %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+        
+class Planetas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(250), nullable=False)
+    diametro = db.Column(db.String(250), nullable=False)
+    periodo_orbital =  db.Column(db.String(250), nullable=False)
+    poblacion =  db.Column(db.String(250), nullable=False)
+    favoritos = db.relationship('Favoritos',backref='planetas', lazy=True)  
+    
+    def __repr__(self):
+        return '<Planetas %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach          
+            }
+        
+class Favoritos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planetas_id = db.Column(db.Integer, db.ForeignKey('planetas.id'))
+    personajes_id = db.Column(db.Integer, db.ForeignKey('personajes.id'))
+    
+    def __repr__(self):
+        return '<Favoritos %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach          
+            }
+            
